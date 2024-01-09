7de5f57ffbb5e482f4e00fd8c350d208a6130306
github_darrellv14 <darrell.valentino14@gmail.com>
2024-01-10 00:57:42 +0700

    Countdown React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { loadConfettiPreset } from 'tsparticles-preset-confetti';
+import { Typewriter } from "react-simple-typewriter";
+import { useState } from "react";
+import Countdown from "react-countdown";
 
 function App() {
+  const [birthdayMessage, setBirthdayMessage] = useState([
+    "Darrell's Birthday Countdown",
+    "Bye 19",
+    "Hello 20!",
+  ]);
+
+  const particleInitialization = async (engine) => {
+    await loadConfettiPreset(engine);
+    await loadFireworksPreset(engine);
+  };
+
+  function timeLeft() {
+    const birthDate = new Date("February 14, 2024 00:00:00").getTime();
+    const nowDate = new Date().getTime();
+    const remainingTime = birthDate - nowDate;
+    return remainingTime;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        init={particleInitialization}
+        options={{ preset: "fireworks" }}
+      />
+      
+      <div className="flex flex-col justify-center items-center min-h-screen gap-4 bg-black">
+        <span className="text-white text-4xl font-bold px-4 z-50">
+          <Typewriter
+            words={birthdayMessage}
+            loop={false}
+            cursorStyle={"✨"}
+            cursor
+          />
+        </span>
+        <div className="z-50 text-white font-bold text-2xl">
+          <Countdown
+            date={Date.now() + timeLeft()}
+            onComplete={() =>
+              setBirthdayMessage([
+                "Happy 20th Birthday",
+                "Darrell",
+                "Valentino!",
+              ])
+            }
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
